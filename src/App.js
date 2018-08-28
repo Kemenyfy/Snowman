820df24d6794479c1a046570e7c9b3b9a655db64
Kemenyfy <gustavo.kemenyfy@gmail.com>
2018-08-28 13:48:39 -0400

    Added Header and Created GameLogic Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Snowman from './images/Snowman.jpg';
+
 import './App.css';
+
+import GameLogic from './Components/GameLogic.js' 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={Snowman} className="App-logo" alt="logo" />
+          <h1 className="App-title">Ultimate Snowman</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <GameLogic />
+        </div>
+
       </div>
     );
   }
